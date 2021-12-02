9806be5438c51892f925ba42805f021a1592085a
Juan Manuel Giron <juan.giron@mi.unc.edu.ar>
2021-12-02 02:08:13 -0300

    clase hilo

---
+++ src/Simulacion/Hilos.java
@@ -0,0 +1,37 @@
+package Simulacion;
+
+import Monitor.Monitor;
+
+public class Hilos implements Runnable{
+    private String nombre;
+    private Monitor monitor;
+    private int secuencia[];
+    /**
+     * Constructor de la clase Hilos
+     * @param nombre nombre del hilo
+     * @param monitor el monitor que ejecutara el metodo disparar
+     * @param secuencia secuencia de transiciones que disparara el hilo
+     */
+    public Hilos(String nombre,Monitor monitor,int secuencia[]) {
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.secuencia = secuencia;
+    }
+    /**
+     * Metodod run
+     */
+    public void run() {
+        while(true) {
+            for(int i=0; i<secuencia.length; i++) {
+                try {
+                    monitor.dispararTransicion(secuencia[i]);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public String getNombre() {
+        return nombre;
+    }
+}
